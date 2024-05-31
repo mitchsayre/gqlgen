ce259118b18800c9517e7c220fc4a86f4c539b65
Gavin Yu <2003jyu@gmail.com>
2024-05-31 16:46:58 -0400

    changed dist/plugin.js file and codegen.ts (make it generated more file).

--- example/codegen.ts
+++ example/codegen.ts
@@ -1,16 +1,54 @@
-import { CodegenConfig } from "@graphql-codegen/cli";
+import { CodegenConfig, generate } from "@graphql-codegen/cli";
+import * as path from 'path';
+import glob from 'glob';
+
+const documentsPath = "./example/operations/**/*.graphql";
+const outputPath = "./example/generated";
+const indexFilePath = "./src/language/TypeScript.ts"; // Path to TypeScript.ts
+
 
 const config: CodegenConfig = {
   schema: "./example/schema.graphql",
-  documents: "./example/operations/**/*.graphql",
-  generates: {
-    "./example/generated/out.txt": {
-      config: {
-        language: "Python", // This part here
-      },
-      plugins: ["./dist/plugin.js"],
-    },
-  },
+  documents: documentsPath,
+  generates: {},
 };
 
+const files = glob.sync(documentsPath).concat([indexFilePath]); // get each file
+
+files.forEach (file=>{
+  let fileName, outputFile;
+  if(file === indexFilePath){
+    fileName = path.basename("index", '.ts');
+    outputFile = path.join(outputPath, `${fileName}.ts`);
+  
+  } // index file (TypeScript.ts)
+  else {
+    fileName = path.basename(file, '.graphql');
+    outputFile = path.join(outputPath, `${fileName}.ts`);
+  }
+
+  config.generates[outputFile] = {
+    config: {
+      language: "Typescript",
+      fName: fileName,
+    },
+    plugins: ["./dist/plugin.js"],
+  };
+});
+
+generate(config);
+
 export default config;
+
+// const config: CodegenConfig = {
+//   schema: "./example/schema.graphql",
+//   documents: "./example/operations/**/*.graphql",
+//   generates: {
+//     "./example/generated/out.txt": {
+//       config: {
+//         language: "Python", // This part here
+//       },
+//       plugins: ["./dist/plugin.js"],
+//     },
+//   },
+// };
