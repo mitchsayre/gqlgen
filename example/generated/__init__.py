a5c7520d1eb5841db4640e9a2ce5b82a9acd68eb
Gavin Yu <2003jyu@gmail.com>
2024-06-24 09:03:34 -0400

    delete generated code

--- example/generated/__init__.py
+++ example/generated/__init__.py
@@ -1,30 +1,47 @@
 import requests, json
-from album_user_posts import AlbumUserPosts
-
+from create_album import CreateAlbum
+from create_photo import CreatePhoto
+from create_post import CreatePost
 
 class GqlgenClient:
-    def __init__(self, config):
-        self.config = config
+  def __init__(self, config):
+    self.config = config
 
-    def execute_operation(self, input, operation_name, document):
-        url = self.config.url
-        headers = self.config.headers
-        body = {
-            "query": document,
-            "operationName": operation_name,
-            "variables": input,
-        }
+  def execute_operation(self, input, operation_name, document):
+    url = self.config.url
+    headers = self.config.headers
+    body = {
+        "query": document,
+        "operationName": operation_name,
+        "variables": input,
+    }
 
-        res = requests.post(url, headers=headers, data=json.dumps(body))
-        return res.json()
+    res = requests.post(url, headers=headers, data=json.dumps(body))
+    return res.json()
+  
+  def create_album(self, input) -> CreateAlbum:
+    res = self.execute_operation(
+      self,
+      input,
+      "CreateAlbum",
+      'mutation CreateAlbum($input: CreateAlbumInput!) {   createAlbum(input: $input) {     id     title     user {       name       email       posts {         links {           first {             limit             page           }           last {             limit             page           }         }       }       todos {         data {           completed           id         }       }     }   } }'
+    )
+    return CreateAlbum.from_dict(res)
 
-    def album_user_posts(self, input) -> AlbumUserPosts:
-        res = self.execute_operation(
-            self,
-            input,
-            "AlbumUserPosts",
-            'query AlbumUserPosts($options: PageQueryOptions) { album(id: "10") { id title user { id email name posts(options: $options) { data { title id } } } } }',
-        )
-        return AlbumUserPosts.to_dict(res)
+  def create_photo(self, input) -> CreatePhoto:
+    res = self.execute_operation(
+      self,
+      input,
+      "CreatePhoto",
+      'mutation CreatePhoto($input: CreatePhotoInput!) {   createPhoto(input: $input) {     url     title     album {       id       title       user {         id         name         phone         todos {           data {             completed             id           }         }       }     }   } }'
+    )
+    return CreatePhoto.from_dict(res)
 
-    # TODO: Implement entrypointPlugin.ts to generate the operation functions like album_user_posts
+  def create_post(self, input) -> CreatePost:
+    res = self.execute_operation(
+      self,
+      input,
+      "CreatePost",
+      'mutation CreatePost($input: CreatePostInput!) {   createPost(input: $input) {     body     id     title   } }'
+    )
+    return CreatePost.from_dict(res)
